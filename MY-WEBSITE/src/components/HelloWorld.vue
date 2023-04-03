9e00df5df9b9e88d342f03244e5a4367f8c4beb5
albyschianni <alberto.schianni@gmail.com>
2023-04-03 18:12:47 +0200

    upgrade & bug fix, V.1 (div section 2 section )

--- MY-WEBSITE/src/components/HelloWorld.vue
+++ MY-WEBSITE/src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <script>
 
 export default {
-  // data() {
-
-  // },
-  // methods: {
-
-  // },
+  name: 'MyButton',
+  data() {
+    return {
+      showDropdown: false
+    }
+  },
 
 }
 
@@ -16,14 +16,21 @@ export default {
   <body id="page-top">
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="#page-top">ALBY</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
-          data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-          aria-label="Toggle navigation">
-          Menu
-          <i class="fas fa-bars"></i>
-        </button>
+      <div class="container px-4 px-lg-5 d-flex flex-nowrap">
+        <a class="navbar-brand" href="#page-top"> <img class="size-logo" src="../assets/alby_logoLisa2.png" alt=""></a>
+
+        <div class="dropdown">
+          <button class="ms-button btn btn-secondary dropdown-toggle " type="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            Menu
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="ms-aButton" href="#about">About</a></li>
+            <li><a class="ms-aButton" href="#projects">Projects</a></li>
+            <li><a class="ms-aButton" href="#contact">Contact</a></li>
+          </ul>
+        </div>
+
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
@@ -50,9 +57,9 @@ export default {
         <div class="row gx-4 gx-lg-5 justify-content-center">
           <div class="col-lg-12 flex-meDev">
             <img class="img-me" src="../assets/myself.jpg" alt="..." />
-            <div style="margin-top: 100px;">
-              <h2 class="text-white mb-4">ALBERTO SCHIANNI</h2>
-              <p class="text-white-50 text-start ms-text-style">
+            <div class="div-name">
+              <h2 class="text-white mb-4 div-ms-medium ">ALBERTO SCHIANNI</h2>
+              <p class="text-white-50 text-start ms-text-style p-ms-medium">
                 Hi, I'm 23 Italian guy. <br>
                 After my graduation in Communication Media & Advertising I decided to study Web Development
                 driven by love for technology & innovation. <br>
@@ -60,29 +67,29 @@ export default {
               </p>
               <div class="d-flex justify-content-around">
                 <div>
-                  <h4 style="color: white;">FRONT-END</h4>
+                  <h4 class="h4-ms-medium" style="color: white;">FRONT-END</h4>
                   <div class="d-flex align-items-center gap-logos">
                     <ul class="text-start">
-                      <li>HTML </li>
-                      <li>CSS </li>
-                      <li>JAVASCRIPT </li>
-                      <li>VUE JS </li>
+                      <li class="li-ms-medium">HTML </li>
+                      <li class="li-ms-medium">CSS </li>
+                      <li class="li-ms-medium">JAVASCRIPT </li>
+                      <li class="li-ms-medium">VUE JS </li>
                     </ul>
 
                   </div>
                 </div>
                 <div>
-                  <h4 style="color: white;">BACK-END</h4>
+                  <h4 class="h4-ms-medium" style="color: white;">BACK-END</h4>
                   <div class="d-flex align-items-center gap-logos">
                     <ul class="text-start">
-                      <li>PHP </li>
-                      <li>MYSQL </li>
-                      <li>LARAVEL </li>
+                      <li class="li-ms-medium">PHP </li>
+                      <li class="li-ms-medium">MYSQL </li>
+                      <li class="li-ms-medium">LARAVEL </li>
                     </ul>
                   </div>
                 </div>
               </div>
-              <div class="ms-text-style text-start" style="margin-top: 30px;">
+              <div class="ms-text-style text-start div-cv-medium" style="margin-top: 30px;">
                 For more info:
                 <a href="https://drive.google.com/drive/folders/1y_l-wsDdOdKuhxG1bb5UIL8Wf-suMuLW?usp=share_link"
                   style="text-decoration: none; color: white;">
@@ -91,79 +98,188 @@ export default {
                 </a>
               </div>
             </div>
-
-
-
           </div>
+        </div>
+      </div>
+    </section>
 
+    <!-- SECTION PROJECTS -->
+
+    <section class="projects-section bg-light project-two-line" id="projects">
+      <div class="container px-4 px-lg-5">
+        <h1 class="h1-projects">
+          PROJECTS
+        </h1>
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+          <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="../assets/spotify.png" alt="..." />
+          </div>
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text text-center text-lg-left">
+              <h4>Airbnb Clone <br>(Front-end & Back-end)</h4>
+              <p class="text-black-50 mb-0">
+                With my team we created an Airbnn replica with many function: full CRUD for data, an advanced
+                research with some filter (TomTom API for location), safe routes inside the app, register & login section,
+                possibility to simulate payment with Braintree, dedicate page for messages from users and statistics to
+                check the apartment'views. <br>
+                - VUE JS / LARAVEL (JAVASCRIPT & PHP)
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text-2 text-center text-lg-left">
+              <h4>Website Landing Page <br>(Front-end)</h4>
+              <p class="text-black-50 mb-0">
+                Full Landing Page graphic, simulation of a website divided in many sections. Created with VUE JS using
+                components structure to mantain the code.
+              </p>
+            </div>
+          </div>
+          <div class="col-xl-8 col-lg-7 image-carousel-ms">
+            <img class="img-fluid mb-3 mb-lg-0" src="../assets/exsite.png" alt="..." />
+          </div>
+        </div>
+
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+          <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="../assets/spotify.png" alt="..." />
+          </div>
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text text-center text-lg-left">
+              <h4>Spotify <br>(Front-end)</h4>
+              <p class="text-black-50 mb-0">
+                Spotify Homepage, replica inspired by famous Music Webapp, full CSS & HTML structure responsive on many
+                size/devices.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text-2 text-center text-lg-left">
+              <h4>Netflix <br>(Front-end / API)</h4>
+              <p class="text-black-50 mb-0">
+                Netflix Homepage, replica inspired by famous Streaming Webapp created with VUE JS for the components
+                structure. Inside the cards it's possible to view some data (Title, Overview,rating..) generated by using
+                "ThemovieDB" API and with the search button you can search inside the Whole catalog the movie you prefer.
+              </p>
+            </div>
+          </div>
+          <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="../assets/netflix.png" alt="..." />
+          </div>
+        </div>
+
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+          <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="../assets/whatsapp.png" alt="..." />
+          </div>
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text text-center text-lg-left">
+              <h4>Whatsapp <br>(Front-end)</h4>
+              <p class="text-black-50 mb-0">
+                Whatsapp Chat-page, replica inspired by famous messaging Webapp created with HTML CSS & JS, you can select
+                the chat you prefer and write a message, the app will respond to you with a default message.
+              </p>
+            </div>
+          </div>
         </div>
 
       </div>
     </section>
-    <!-- Projects -->
-    <!-- WORK IN PROGRESS WEBSITE -->
-    <section id="projects">
-    <div class="mt-5">
-        <h2 class="text-center style-WP">The Website is Work in progress, sorry for inconvenience section "PROJECTS" and
-          some
-        other details will be uploaded in a couple of days.
-        I'm currently working on it! <br>
-        Come to check it out when it will be done!
-      </h2>
-    </div>
-    <div class="d-flex justify-content-center">
-      <img class="img-me" src="../assets/Work-in-progress.png" />
-    </div>
-  </section>
 
-    <!-- <section class="projects-section bg-light" id="projects">
-                                                                                                                                                                                                                                            <div class="container px-4 px-lg-5"> -->
-  <!-- Featured Project Row-->
-  <!-- <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-          <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/bg-masthead.jpg" alt="..." />
-          </div>
+    <!-- SECTION PROJECTS 2-->
+
+    <section class="projects-section bg-light project-one-line" id="projects">
+      <div class="container px-4 px-lg-5">
+        <h1 class="h1-projects">
+          PROJECTS
+        </h1>
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+
           <div class="col-xl-4 col-lg-5">
             <div class="featured-text text-center text-lg-left">
-              <h4>Shoreline</h4>
-              <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any
-                project - even commercial projects! Download it, customize it, and publish your website!</p>
+              <h4>Airbnb Clone <br>(Front-end & Back-end)</h4>
+              <p class="text-black-50 mb-4">
+                With my team we created an Airbnn replica with many function: full CRUD for data, an advanced
+                research with some filter (TomTom API for location), safe routes inside the app, register & login section,
+                possibility to simulate payment with Braintree, dedicate page for messages from users and statistics to
+                check the apartment'views. <br>
+                - VUE JS / LARAVEL (JAVASCRIPT & PHP)
+              </p>
+            </div>
+            <div class="col-xl-8 col-lg-7">
+              <img class="img-fluid mb-3 mb-lg-0" src="../assets/spotify.png" alt="..." />
             </div>
           </div>
-            </div> -->
-  <!-- Project One Row-->
-  <!-- <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-01.jpg" alt="..." /></div>
-                                                                                                                                                                                                                                                <div class="col-lg-6">
-                                                                                                                                                                                                                                        <div class="bg-black text-center h-100 project">
-              <div class="d-flex h-100">
-                <div class="project-text w-100 my-auto text-center text-lg-left">
-                  <h4 class="text-white">Misty</h4>
-                  <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else,
-                    along with a description.</p>
-                  <hr class="d-none d-lg-block mb-0 ms-0" />
-                </div>
-              </div>
+        </div>
+
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text-2 text-center text-lg-left">
+              <h4>Website Landing Page <br>(Front-end)</h4>
+              <p class="text-black-50 mb-4">
+                Full Landing Page graphic, simulation of a website divided in many sections. Created with VUE JS using
+                components structure to mantain the code.
+              </p>
             </div>
           </div>
-        </div> -->
-  <!-- Project Two Row-->
-  <!-- <div class="row gx-0 justify-content-center">
-              <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-02.jpg" alt="..." /></div>
-              <div class="col-lg-6 order-lg-first">
-                          <div class="bg-black text-center h-100 project">
-                                                                                                                                                                                                                                                        <div class="d-flex h-100">
-                                                                                                                                                                                                                                                          <div class="project-text w-100 my-auto text-center text-lg-right">
-                                                                                                                                                                                                                                                            <h4 class="text-white">Mountains</h4>
-                                                                                                                                                                                                                                                            <p class="mb-0 text-white-50">Another example of a project with its respective description. These
-                                                                                                                                                                                                                                                              sections work well responsively as well, try this theme on a small screen!</p>
-                                                                                                                                                                                                                                                            <hr class="d-none d-lg-block mb-0 me-0" />
-                                                                                                                                                                                                                                                          </div>
-                                                                                                                                                                                                                                                        </div>
-                                                                                                                                                                                                                                                      </div>
-                                                                                                                                                                                                                                                    </div>
-                                                                                                                                                                                                                                                  </div>
-                                                                                                                                                                                                                                                </div>
-                                                                                                                                                                                                                                              </section> -->
+          <div class="col-xl-8 col-lg-7 image-carousel-ms">
+            <img class="img-fluid mb-3 mb-lg-0" src="../assets/exsite.png" alt="..." />
+          </div>
+        </div>
+
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text text-center text-lg-left">
+              <h4>Spotify <br>(Front-end)</h4>
+              <p class="text-black-50 mb-4">
+                Spotify Homepage, replica inspired by famous Music Webapp, full CSS & HTML structure responsive on many
+                size/devices.
+              </p>
+            </div>
+            <div class="col-xl-8 col-lg-7">
+              <img class="img-fluid mb-3 mb-lg-0" src="../assets/spotify.png" alt="..." />
+            </div>
+          </div>
+        </div>
+
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text-2 text-center text-lg-left">
+              <h4>Netflix <br>(Front-end / API)</h4>
+              <p class="text-black-50 mb-4">
+                Netflix Homepage, replica inspired by famous Streaming Webapp created with VUE JS for the components
+                structure. Inside the cards it's possible to view some data (Title, Overview,rating..) generated by
+                using
+                "ThemovieDB" API and with the search button you can search inside the Whole catalog the movie you
+                prefer.
+              </p>
+            </div>
+            <div class="col-xl-8 col-lg-7">
+              <img class="img-fluid mb-3 mb-lg-0" src="../assets/netflix.png" alt="..." />
+            </div>
+          </div>
+        </div>
+
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text text-center text-lg-left">
+              <h4>Whatsapp <br>(Front-end)</h4>
+              <p class="text-black-50 mb-4">
+                Whatsapp Chat-page, replica inspired by famous messaging Webapp created with HTML CSS & JS, you can select
+                the chat you prefer and write a message, the app will respond to you with a default message.
+              </p>
+            </div>
+            <div class="col-xl-8 col-lg-7">
+              <img class="img-fluid mb-3 mb-lg-0" src="../assets/whatsapp.png" alt="..." />
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
 
     <!-- Contact-->
     <section class="contact-section bg-black padding-footer" id="contact">
